d8dc2ae6ab64f627bac5ef68662b9cd770104ff9
Fantaly <ragusiemanuel@gmail.com>
2019-01-21 18:17:41 +0100

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -3,34 +3,35 @@
 #include <string>
 #include <sstream>
 #include <Windows.h>
-
 #undef max
 
 using namespace std;
-void spawnThreads(HWND hwnd);
-bool Process_change(HWND hwnd_id);
-int Process_Check_1(HWND hwnd);
-int Process_Check(HWND hwnd);
+void spawnThreads(HWND hwnd, DWORD ProcID, HANDLE handle);
+bool Process_change(HWND hwnd, DWORD ProcID, HANDLE handle);
+int Process_Check_1(HWND hwnd, DWORD ProcID, HANDLE handle);
+int Process_Check(HWND hwnd, DWORD ProcID, HANDLE handle);
 
 int main()
 {
+	DWORD ProcID = NULL;
 	HWND hwnd = FindWindowA(NULL, "C:\\Users\\Manu\\source\\repos\\ConsoleApplication1\\Debug\\ConsoleApplication1.exe");
-	spawnThreads(hwnd);
-	Process_Check_1(hwnd);
+	GetWindowThreadProcessId(hwnd, &ProcID);
+	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcID);
+	spawnThreads(hwnd, ProcID, handle);
 	return 0;
 }
 
-void spawnThreads(HWND hwnd)
+void spawnThreads(HWND hwnd, DWORD ProcID, HANDLE handle)
 {
 	thread threads[2];
-	threads[0] = thread(Process_Check_1, hwnd);
-	threads[1] = thread(Process_Check, hwnd);
+	threads[0] = thread(Process_Check_1, hwnd, ProcID, handle);
+	threads[1] = thread(Process_Check, hwnd, ProcID, handle);
 	for (auto& th : threads) 
 		th.join();
 
 }
 
-int Process_Check_1(HWND hwnd)
+int Process_Check_1(HWND hwnd, DWORD ProcID, HANDLE handle)
 {
 	if (hwnd == NULL)
 	{
@@ -40,20 +41,22 @@ int Process_Check_1(HWND hwnd)
 	}
 	else
 	{
-		Process_change(hwnd);
+		Process_change(hwnd, ProcID, handle);
 		return 1;
 	}
 	return 1;
 }
 
-int Process_Check(HWND hwnd)
+int Process_Check(HWND hwnd, DWORD ProcID, HANDLE handle)
 {
+	DWORD exitCode = 0;
 	while (1) 
 	{
-		HWND hwnd = FindWindowA(NULL, "C:\\Users\\Manu\\source\\repos\\ConsoleApplication1\\Debug\\ConsoleApplication1.exe");
-		if (hwnd == NULL)
+		GetExitCodeProcess(handle, &exitCode);
+		if (exitCode != STILL_ACTIVE)
 		{
-			cout << "No window finded." << endl;
+			system("cls");
+			cout << "Processo terminato." << endl;
 			Sleep(3000);
 			exit(-1);
 		}
@@ -61,24 +64,23 @@ int Process_Check(HWND hwnd)
 	return 0;
 }
 
-bool Process_change(HWND hwnd_id)
+bool Process_change(HWND hwnd, DWORD ProcID, HANDLE handle)
 {
 	/* Start declaration*/
 	stringstream str;
-	SIZE_T Size_n = sizeof(int);
-	DWORD ProcID = NULL;
 	int addr = 0;
 	string addr_string = "";
 	/* End declaration*/
+
 	cout << "Insert memory address: ";
 	cin >> addr_string;
+
 	/*Conversion*/
 	str << addr_string; /* Conversione string esadecimale */ str >> std::hex >> addr;
 	/*END Conversion*/
+
 	LPVOID address = (VOID*)addr; //Assigned value of addr as an address to be pointed
 	cout << address;
-	GetWindowThreadProcessId(hwnd_id, &ProcID);
-	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcID);
 	if (ProcID == NULL)
 	{
 		cout << "Cannot obtain the process." << endl;
@@ -91,14 +93,14 @@ bool Process_change(HWND hwnd_id)
 		int value_in = 0;
 		while (1)
 		{
-			ReadProcessMemory(handle, address, &value_out, Size_n, 0);
+			ReadProcessMemory(handle, address, &value_out, sizeof(int), 0);
 			cout << "\n\nValore attuale: " << value_out;
 			cout << "\nInserisci valore: ";
 			cin >> value_in;
 			if (!(cin.fail()))
 			{
 				cout << endl;
-				WriteProcessMemory(handle, address, &value_in, Size_n, 0); // HANDLE - ADRESS - VALORE - SIZE INTEGER
+				WriteProcessMemory(handle, address, &value_in, sizeof(int), 0); // HANDLE - ADRESS - VALORE - SIZE INTEGER
 			}
 			else if (cin.fail()){
 				cout << "Tipo di valore errato" << endl;
